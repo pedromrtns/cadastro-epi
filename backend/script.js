0af1981888f3354e03f9ad0d35db586c70d0e4a2
pedromrtns <pcruz6064@gmail.com>
2024-02-13 10:38:21 -0300

    Play js

---
+++ backend/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('entrarBtn').addEventListener('click', function(event){
+        var email = document.getElementById('ilogin').value;
+        var login = document.getElementById('ipassword').value;
+    })
+});
