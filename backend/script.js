c77e5c60f91e33b40ede4b888da82a98751bbc8d
pedromrtns <pcruz6064@gmail.com>
2024-02-17 13:56:42 -0300

    atualização

--- backend/script.js
+++ backend/script.js
@@ -1,6 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('entrarBtn').addEventListener('click', function(event){
-        var email = document.getElementById('ilogin').value;
-        var login = document.getElementById('ipassword').value;
-    })
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // Evita o envio do formulário
+
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+        
+        if (email === "pedro.martins@sigma.com" && password === "123") {
+            alert("Login bem-sucedido!");
+            window.location.href = "home.html";
+        } else {
+            alert("E-mail ou senha inválidos.");
+        }
+    });
 });
+
+
+ 
